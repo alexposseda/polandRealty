34baeb9bf1c420cdb5bb889dd415be20a6ece4dc
alexposseda <alexposseda@gmail.com>
2016-10-12 12:56:21 +0300

    add config (urlManager, localeUrls, i18n) and translations files

--- common/config/main.php
+++ common/config/main.php
@@ -1,12 +1,36 @@
 <?php
     return [
-        'vendorPath' => dirname(dirname(__DIR__)).'/vendor',
-        'components' => [
+        'sourceLanguage' => 'en',
+        'vendorPath'     => dirname(dirname(__DIR__)).'/vendor',
+        'components'     => [
             'cache'       => [
                 'class' => 'yii\caching\FileCache',
             ],
             'authManager' => [
                 'class' => 'yii\rbac\DbManager',
             ],
+            'urlManager'  => [
+                'class'     => 'codemix\localeurls\UrlManager',
+                'languages' => [
+                    'en',
+                    'pl'
+                ],
+            ],
+            'i18n'        => [
+                'translations' => [
+                    'app'   => [
+                        'class'    => 'yii\i18n\PhpMessageSource',
+                        'basePath' => '@common/translations',
+                    ],
+                    'info'  => [
+                        'class'    => 'yii\i18n\PhpMessageSource',
+                        'basePath' => '@common/translations',
+                    ],
+                    'error' => [
+                        'class'    => 'yii\i18n\PhpMessageSource',
+                        'basePath' => '@common/translations',
+                    ],
+                ],
+            ]
         ],
     ];
