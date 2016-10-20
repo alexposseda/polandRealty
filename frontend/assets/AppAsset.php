99fe181ed65bf9399f19c995160ce7328b40b2f8
alexposseda <alexposseda@gmail.com>
2016-10-20 16:43:39 +0300

    add materialize asset

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,6 +18,7 @@ class AppAsset extends AssetBundle
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'macgyer\yii2materializecss\assets\MaterializeAsset',
+        'macgyer\yii2materializecss\assets\MaterializePluginAsset',
     ];
 }
