78bff866e62d72a5c033d10ce3f9987e4dc09d60
SergSova <sergeysova@ukr.net>
2016-10-20 17:31:15 +0300

    tmp commit

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -4,25 +4,55 @@
 
     use common\models\search\RealtySearch;
     use Yii;
+    use yii\filters\AccessControl;
     use yii\web\Controller;
 
     class UserController extends Controller{
+        /**
+         * @inheritdoc
+         */
+        public function behaviors(){
+            return [
+                'access' => [
+                    'class' => AccessControl::className(),
+                    'rules' => [
+                        [
+                            'actions' => ['view'],
+                            'allow'   => true,
+                            'roles'   => ['?'],
+                        ],
+                        [
+                            'allow' => true,
+                            'roles' => ['@'],
+                        ],
+                    ],
+                ],
+                //                'verbs'  => [
+                //                    'class'   => VerbFilter::className(),
+                //                    'actions' => [
+                //                        'delete' => ['post'],
+                //                    ],
+                //                ],
+            ];
+        }
+
         public function actionIndex(){
-            //todo выводим список обьявлений пользователя
             $searchModel = new RealtySearch();
-            $searchModel->created_by = Yii::$app->user->id;
+            if(!Yii::$app->authManager->checkAccess(Yii::$app->user->id, 'manager')){
+                $searchModel->created_by = Yii::$app->user->id;
+            }
             $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
             return $this->render('index', ['dataProvider' => $dataProvider, 'searchModel' => $searchModel]);
         }
 
         public function actionProfile(){
-            //todo страница для изменения персональных данных
             $model = Yii::$app->user->identity;
 
-            if($model->load(Yii::$app->request->post())&&$model->save()){
+            if($model->load(Yii::$app->request->post()) && $model->save()){
                 return $this->goHome();
             }
+
             return $this->render('profile', ['model' => $model]);
         }
 
